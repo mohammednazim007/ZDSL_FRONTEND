e9730868771893f8b77bd3a8c5f7c8ff82b57fae
mohammednazim007 <mohammednazim3629@gmail.com>
2025-05-16 17:35:09 +0600

    Remove sensitive environment variables from .env.local and migrate to secrets.ts for better security practices

---
+++ src/app/secrets/secrets.ts
@@ -0,0 +1,18 @@
+// NEXT_PUBLIC_BASE_URL="https://zdsl.com.bd/api/endpoint/graphql"
+// NEXT_PUBLIC_MEDIA_URL="https://zdsl.com.bd/api/endpoint/media"
+// NEXT_PUBLIC_MEDIA_UPLOAD_URL="https://zdsl.com.bd/api/media/upload"
+// NEXT_PUBLIC_ORIGIN="https://zdsl.com.bd/api/callback"
+
+// NEXT_BASE_DATA_FATCHING="https://zdsl.com.bd"
+// GRAPHQL_ENDPOINT="https://zdsl.com.bd/api/endpoint/graphql"
+
+// GOOGLE_CLIENT_ID="839715849549-vop0nmc5kfcnv9qse2prcblncrqk51jn.apps.googleusercontent.com"
+// GOOGLE_CLIENT_SECRET="your-google-client-secret"
+
+// FACEBOOK_CLIENT_ID="496076950144317"
+// FACEBOOK_CLIENT_SECRET="fa55a4b9bcdac30e5c639ef9c35acd50"
+
+// NEXTAUTH_URL="https://zdsl.com.bd"
+
+// EMAIL_PASS="toif ylmw ujta lxiz"
+// STP_EMAIL="altaj1019@gmail.com"
